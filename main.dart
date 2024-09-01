3d34ab316ab21a454c845ad57eb450ec01668ded
Hashim Hayat <hh1316@nyu.edu>
2024-08-31 21:03:27 -0400

    feat: create a detailed readme file this Flutter Project

--- main.dart
+++ main.dart
@@ -1,58 +1,68 @@
-```
 /// The main function of the app.
 Future<void> main() async {
-  /// Initialize Sentry with the specified DSN and release.
-  await SentryFlutter.init(
-    (options) {
-      options.dsn = sentryDsn;
-      options.release = "1.0.0+1";
-    },
-    /// Run the app after Sentry initialization.
-    appRunner: () => runApp(MyApp()),
-  );
+  await runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    FlutterError.onError = (details) {
+      FlutterError.presentError(details);
+      Sentry.captureException(details.exception, stackTrace: details.stack);
+    };
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = sentryDsn;
+        options.release = "1.0.0+1";
+      },
+      appRunner: () => runApp(MyApp()),
+    );
+  }, (error, stackTrace) {
+    Sentry.captureException(error, stackTrace: stackTrace);
+  });
 }
 
 /// The main widget of the app.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    /// Get the device information.
-    final deviceInfo = DeviceInfoPlugin();
-    final androidInfo = await deviceInfo.androidInfo;
-
-    /// Set the status bar color.
-    FlutterStatusbarcolor.setStatusBarColor(Colors.blue);
-
     return MaterialApp(
       title: 'Stacked Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Stacked Demo'),
-        ),
-        body: Column(
-          children: <Widget>[
-            /// Display a text.
-            Text('Hello, world!'),
+      home: MyHomePage(),
+    );
+  }
+}
 
-            /// Create a gesture detector with a blue container.
-            SizedBox.square(
-              dimension: 100,
-              child: GestureDetector(
-                behavior: HitTestBehavior.opaque,
-                onTap: () {
-                  /// Log an event to Firebase Analytics.
-                  analytics.logEvent(name: 'button_tapped');
-                },
-                child: Container(
-                  color: Colors.blue.shade100,
-                ),
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final deviceInfo = DeviceInfoPlugin();
+    final androidInfo = await deviceInfo.androidInfo;
+    FlutterStatusbarcolor.setStatusBarColor(Colors.blue);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stacked Demo'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('Hello, world!'),
+          SizedBox.square(
+            dimension: 100,
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                analytics.logEvent(name: 'button_tapped');
+              },
+              child: Container(
+                color: Colors.blue.shade100,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
