b70ce3ffbcc5e1adeb820829ae8f6137aaa4ad98
Hashim Hayat <hh1316@nyu.edu>
2024-08-31 21:12:07 -0400

    feat: create a detailed readme file this Flutter Project

--- main.dart
+++ main.dart
@@ -1,6 +1,99 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
-import 'package:google_analytics/google_analytics.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  final androidDeviceInfo = await deviceInfo.androidInfo;
+  Sentry.init(
+    (options) {
+      options.dsn =
+          'https://f95f8f2560394706b0045a45f105074a@o452924.ingest.sentry.io/5465977';
+      options.release = androidDeviceInfo.version.release;
+      options.dist = androidDeviceInfo.model;
+    },
+  );
+
+  GaService.init();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterStatusbarcolor.setStatusBarColor(Colors.transparent);
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+class GaService {
+  static GoogleAnalytics _ga;
+
+  static init() async {
+    await GoogleAnalytics.init('UA-ID');
+    _ga = await GoogleAnalytics.instance;
+  }
+}
